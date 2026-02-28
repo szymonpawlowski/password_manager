778ad7bc6a3c21b9cf457ed79794512be838c58d
szymonpawlowski <szymon.pawlowski.social@gmail.com>
2026-02-28 18:17:06 +0100

    Variable getters

--- PasswordEntry.h
+++ PasswordEntry.h
@@ -14,6 +14,11 @@ struct PasswordEntry
 
     PasswordEntry(string s, string u, string l, string p)
         : serviceName(s), username(u), login(l), password(p) {};
+
+    string getServiceName() const { return serviceName; }
+    string getUsername() const { return username; }
+    string getLogin() const { return login; }
+    string getPassword() const {return password; }
 };
 
 
